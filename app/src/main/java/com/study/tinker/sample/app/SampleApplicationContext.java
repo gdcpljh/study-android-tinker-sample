de8a5f2dcb1df7c03d554449e20d5fa5fb2c61a4
ASLai <gdcpljh@gmail.com>
2020-05-14 14:50:22 +0800

    增加Tinker用例代码

---
+++ app/src/main/java/com/study/tinker/sample/app/SampleApplicationContext.java
@@ -0,0 +1,25 @@
+package com.study.tinker.sample.app;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * <pre>
+ *
+ *      作 者 :        ASLai(laijianhua@ruixiude.com).
+ *      日 期 :        2020/5/13
+ *      版 本 :        V1.0
+ *      描 述 :        description
+ *
+ *
+ * </pre>
+ *
+ * @author ASLai
+ */
+public class SampleApplicationContext {
+
+    public static Application application = null;
+
+    public static Context context = null;
+
+}
